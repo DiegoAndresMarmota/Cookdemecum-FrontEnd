5bbc3db8aea02fb9c405544e95a776f0f89dfc5e
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-11-09 21:23:02 -0300

    Endpoints de userConstants

---
+++ src/constants/userConstants.jsx
@@ -0,0 +1,30 @@
+//[GET] Login Usuario
+export const USER_LOGIN_REQUEST = "USER_LOGIN_REQUEST";
+export const USER_LOGIN_SUCCESS = "USER_LOGIN_SUCCESS";
+export const USER_LOGIN_FAIL = "USER_LOGIN_SUCCESS";
+
+//[GET] Log Out Usuario
+export const USER_LOGOUT = "USER_LOGOUT";
+
+//[POST] Registrar Nuevo Usuario
+export const USER_REGISTER_REQUEST = "USER_REGISTER_REQUEST";
+export const USER_REGISTER_SUCCESS = "USER_REGISTER_SUCCESS";
+export const USER_REGISTER_FAIL = "USER_REGISTER_FAIL";
+
+//[PUT] Editar comentario del usuario
+export const USER_EDIT_REQUEST = "USER_EDIT_REQUEST";
+export const USER_EDIT_SUCCESS = "USER_EDIT_SUCCESS";
+export const USER_EDIT_FAIL = "USER_EDIT_FAIL";
+export const USER_EDIT_RESET = "USER_EDIT_RESET";
+
+//[POST] Usuario unico
+export const USER_SOLO_REQUEST = "USER_SOLO_REQUEST";
+export const USER_SOLO_SUCCESS = "USER_SOLO_SUCCESS";
+export const USER_SOLO_FAIL = "USER_SOLO_FAIL";
+export const USER_SOLO_RESET = "USER_SOLO_RESET";
+
+//[POST] Usuarios en general
+export const USER_LIST_REQUEST = "USER_LIST_REQUEST";
+export const USER_LIST_SUCCESS = "USER_LIST_SUCCESS";
+export const USER_LIST_FAIL = "USER_LIST_FAIL";
+export const USER_LIST_RESET = "USER_LIST_RESET";
