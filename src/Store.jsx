5d2113fd5f24c94d03493d29d2b1bf106a2596e0
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-11-09 21:34:50 -0300

    Importación del userLoginReducer al estado inicial, cdo. el usuario se logea

--- src/Store.jsx
+++ src/Store.jsx
@@ -1,3 +1,9 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+
+import { userLoginReducer } from "./reducers/userReducers";
+
+const reducer = combineReducers({
+  userLogin: userLoginReducer,
+});
