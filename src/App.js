60d6d69b9d351208e070d8644bc50f8e4e87fbd6
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-11-11 17:56:57 -0300

    Importación de  Route/Register en BrowserRouter

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 //Componentes
-import { Landing } from "./components/Landing";
-import { Header } from "./components/Header";
+import Landing from "./components/Landing";
+import Header from "./components/Header";
 import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/landing" element={<Landing />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
