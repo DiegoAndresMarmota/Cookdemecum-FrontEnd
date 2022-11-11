905de354e17836e8db5af4ebcbbb4a7e9b83b5a3
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-11-11 17:09:56 -0300

    Edición de plantilla de Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -26,92 +26,92 @@ export default function Login() {
             navigate(path);
         }
     }, [userInfo]);
-}
 
-return (
-  <>
-    {error && <Messages>{error}</Messages>}
-    {loading ? (
-      <Loader />
-    ) : (
-      <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <div className="w-full max-w-md space-y-8">
-          <div>
-            <img className="mx-auto h-12 w-auto" src={u} alt="Your Company" />
-            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-              Ingresa a tu cuenta
-            </h2>
-          </div>
-          <form
-            onSubmit={submitHandler}
-            className="mt-8 space-y-6"
-            action="#"
-            method="POST"
-          >
-            <input type="hidden" name="remember" defaultValue="true" />
-            <div className="-space-y-px rounded-md shadow-sm">
-              <div className="my-8">
-                <label htmlFor="email-address" className="sr-only">
-                  Dirección de E-mail
-                </label>
-                <input
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                  placeholder="Email address"
-                />
-              </div>
-              <div>
-                <label htmlFor="password" className="sr-only">
-                  Contraseña
-                </label>
-                <input
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                  placeholder="Password"
-                />
-              </div>
-            </div>
+    return (
+        <>
+            {error && <Messages>{error}</Messages>}
+            {loading ? (
+                <Loader />
+            ) : (
+                <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+                    <div className="w-full max-w-md space-y-8">
+                        <div>
+                            <img className="mx-auto h-12 w-auto" src={u} alt="Your Company" />
+                            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+                                Ingresa a tu cuenta
+                            </h2>
+                        </div>
+                        <form
+                            onSubmit={submitHandler}
+                            className="mt-8 space-y-6"
+                            action="#"
+                            method="POST"
+                        >
+                            <input type="hidden" name="remember" defaultValue="true" />
+                            <div className="-space-y-px rounded-md shadow-sm">
+                                <div className="my-8">
+                                    <label htmlFor="email-address" className="sr-only">
+                                        Dirección de E-mail
+                                    </label>
+                                    <input
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                        id="email"
+                                        name="email"
+                                        type="email"
+                                        autoComplete="email"
+                                        required
+                                        className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                                        placeholder="Email address"
+                                    />
+                                </div>
+                                <div>
+                                    <label htmlFor="password" className="sr-only">
+                                        Contraseña
+                                    </label>
+                                    <input
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        id="password"
+                                        name="password"
+                                        type="password"
+                                        autoComplete="current-password"
+                                        required
+                                        className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                                        placeholder="Password"
+                                    />
+                                </div>
+                            </div>
 
-            <div className="flex items-center justify-between">
-              <div className="text-sm">
-                <a
-                  href="/register"
-                  className="font-medium text-indigo-600 hover:text-indigo-500"
-                >
-                  Registrate
-                </a>
-              </div>
-            </div>
+                            <div className="flex items-center justify-between">
+                                <div className="text-sm">
+                                    <a
+                                        href="/register"
+                                        className="font-medium text-indigo-600 hover:text-indigo-500"
+                                    >
+                                        Registrate
+                                    </a>
+                                </div>
+                            </div>
 
-            <div>
-              <button
-                type="submit"
-                className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-              >
-                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                  <LockClosedIcon
-                    className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-                    aria-hidden="true"
-                  />
-                </span>
-                Logueate
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
-    )}
-  </>
-);
+                            <div>
+                                <button
+                                    type="submit"
+                                    className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                                >
+                                    <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                                        <LockClosedIcon
+                                            className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                                            aria-hidden="true"
+                                        />
+                                    </span>
+                                    Logueate
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            )}
+        </>
+    )
+};
