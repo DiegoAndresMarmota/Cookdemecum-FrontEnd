a6a91d22a838be9142e1ab19e092b8832f497397
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-11-16 14:05:03 -0300

    Creación de EditProfile

---
+++ src/components/EditProfile.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { useNavigate } from "react-router";
+import Messages from "./Messages";
+import Loader from "./Loader";
+import { getSoloUser, editUser } from "../actions/userActions";
+import { USER_EDIT_RESET } from "../constants/userConstants";
+
+export default function EditProfile() {
+
+}
+
+return (
+    
+)
