e3ccfe8ec1164d3dd2ca48abf98d8b1866adfdfe
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-11-16 20:09:34 -0300

    Import icono {user} en Login, Register

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -5,6 +5,7 @@ import { LockClosedIcon } from "@heroicons/react/20/solid";
 import { useNavigate } from "react-router";
 import Messages from "./Messages";
 import Loader from "./Loader";
+import user from '../media/user.png';
 
 export default function Register() {
   const [user_name, setUser_name] = useState("");
@@ -47,7 +48,11 @@ export default function Register() {
         <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
           <div className="w-full max-w-md space-y-8">
             <div>
-              <img className="mx-auto h-12 w-auto" src={u} alt="Your Company" />
+              <img
+                className="mx-auto h-12 w-auto"
+                src={user}
+                alt="Your Company"
+              />
               <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
                 Ingresa a tu cuenta
               </h2>
