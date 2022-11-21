128307d6640cb503525b0e165f1a708e1340fc8f
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-11-20 23:28:18 -0300

    npm unistall autoprefixer y postcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    colors: {
-      white: "#ffffff",
-      silver: "#ecebff",
-      black: "#18181b",
-    },
-    extend: {
-      sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
-    },
+    extend: {},
   },
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [],
 };
