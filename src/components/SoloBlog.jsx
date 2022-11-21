4455a19005ec425507ac8c04980ee21c11e96f60
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-11-20 22:47:40 -0300

    Corrección de createBlogAction por createBlogComment

--- src/components/SoloBlog.jsx
+++ src/components/SoloBlog.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import { blogActionDetails, createBlogAction } from "../actions/blogActions";
+import { blogActionDetails, createBlogComment } from "../actions/blogActions";
 import { getListUsers } from "../actions/userActions";
 import Messages from "./Messages";
 import Loader from "./Loader";
