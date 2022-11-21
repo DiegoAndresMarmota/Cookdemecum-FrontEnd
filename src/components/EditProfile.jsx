3325dc496b724aef2edb5ce3b673caa25e80548b
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-11-20 22:19:48 -0300

    Corrección del } extra, antes del return

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -31,7 +31,7 @@ export default function EditProfile() {
   useEffect(() => {
     if (userInfo.id !== user.id) {
       dispatch({ type: USER_EDIT_RESET });
-      dispatch(getSoloUser("SoloUser"));
+      dispatch(getSoloUser("userProfile"));
     } else {
       setUserName(user.user_name);
       setEmail(user.email);
@@ -89,140 +89,140 @@ export default function EditProfile() {
       setUploading(false);
     }
   };
-}
 
-return (
-  <>
-    {loading ?
-      <Loader />
-      : error
-        ? <Messages>{error}</Messages>
-    : (
-      <div>
-        {message && <Messages>{message}</Messages>}
-        {error && <Messages>{error}</Messages>}
+  return (
+    <>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Messages>{error}</Messages>
+      ) : (
+        <div>
+          {message && <Messages>{message}</Messages>}
+          {error && <Messages>{error}</Messages>}
 
-        <div className="md:grid md:grid-cols-4 md:gap-6">
-          <div className="md:col-span-1"></div>
-          <div className="mt-5 md:col-span-2 md:mt-0">
-            <form action="#" method="POST" onSubmit={submitHandler}>
-              <div className="shadow sm:overflow-hidden sm:rounded-md">
-                <div className="space-y-6 bg-white px-4 py-5 sm:p-6">
-                  <div className="grid grid-cols-3 gap-6">
-                    <div className="col-span-3 sm:col-span-2">
-                      <div className="mt-1 flex rounded-md shadow-sm">
-                        <input
-                          value={user_name}
-                          onChange={(e) => setUserName(e.target.value)}
-                          type="text"
-                          id="user_name"
-                          className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                          placeholder="Full Name"
-                        />
+          <div className="md:grid md:grid-cols-4 md:gap-6">
+            <div className="md:col-span-1"></div>
+            <div className="mt-5 md:col-span-2 md:mt-0">
+              <form action="#" method="POST" onSubmit={submitHandler}>
+                <div className="shadow sm:overflow-hidden sm:rounded-md">
+                  <div className="space-y-6 bg-white px-4 py-5 sm:p-6">
+                    <div className="grid grid-cols-3 gap-6">
+                      <div className="col-span-3 sm:col-span-2">
+                        <div className="mt-1 flex rounded-md shadow-sm">
+                          <input
+                            value={user_name}
+                            onChange={(e) => setUserName(e.target.value)}
+                            type="text"
+                            id="user_name"
+                            className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                            placeholder="Full Name"
+                          />
+                        </div>
+                        <br></br>
+
+                        <div className="mt-1 flex rounded-md shadow-sm">
+                          <input
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            type="email"
+                            name="email"
+                            id="email"
+                            className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                            placeholder="E-mail"
+                          />
+                        </div>
+
+                        <br></br>
                       </div>
-                      <br></br>
-
-                      <div className="mt-1 flex rounded-md shadow-sm">
-                        <input
-                          value={email}
-                          onChange={(e) => setEmail(e.target.value)}
-                          type="email"
-                          name="email"
-                          id="email"
-                          className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                          placeholder="E-mail"
-                        />
-                      </div>
-
-                      <br></br>
                     </div>
-                  </div>
 
-                  <div>
+                    <div>
+                      <label
+                        htmlFor="about"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        About
+                      </label>
+                      <div className="mt-1">
+                        <textarea
+                          type="text"
+                          value={bio}
+                          onChange={(e) => setBio(e.target.value)}
+                          id="bio"
+                          name="bio"
+                          rows={3}
+                          className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                          placeholder="About You"
+                          defaultValue={""}
+                        />
+                      </div>
+                    </div>
+                    <br></br>
                     <label
                       htmlFor="about"
                       className="block text-sm font-medium text-gray-700"
                     >
-                      About
+                      Actualizar tu contraseña
                     </label>
-                    <div className="mt-1">
-                      <textarea
-                        type="text"
-                        value={bio}
-                        onChange={(e) => setBio(e.target.value)}
-                        id="bio"
-                        name="bio"
-                        rows={3}
+                    <div className="mt-1 flex rounded-md shadow-sm">
+                      <input
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        type="password"
+                        id="password"
                         className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                        placeholder="About You"
-                        defaultValue={""}
+                        placeholder="Password"
                       />
                     </div>
-                  </div>
-                  <br></br>
-                  <label
-                    htmlFor="about"
-                    className="block text-sm font-medium text-gray-700"
-                  >
-                    Actualizar tu contraseña
-                  </label>
-                  <div className="mt-1 flex rounded-md shadow-sm">
-                    <input
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                      type="password"
-                      id="password"
-                      className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                      placeholder="Password"
-                    />
-                  </div>
 
-                  <div className="mt-1 flex rounded-md shadow-sm">
-                    <input
-                      value={confirmPassword}
-                      onChange={(e) => setConfirmPassword(e.target.value)}
-                      type="password"
-                      id="confirmPassword"
-                      className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                      placeholder="Confirm Password"
-                    />
-                  </div>
+                    <div className="mt-1 flex rounded-md shadow-sm">
+                      <input
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        type="password"
+                        id="confirmPassword"
+                        className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                        placeholder="Confirm Password"
+                      />
+                    </div>
 
-                  <form>
-                    <label
-                      htmlFor="about"
-                      className="block text-sm font-medium text-gray-700"
+                    <form>
+                      <label
+                        htmlFor="about"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Imagen
+                      </label>
+
+                      <input
+                        type="text"
+                        placeholder="Image"
+                        value={image}
+                        onChange={(e) => setImage(e.target.value)}
+                      ></input>
+
+                      <input
+                        label="Choose file"
+                        type="file"
+                        onChange={uploadFileHandler}
+                      ></input>
+                    </form>
+                  </div>
+                  <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                    <button
+                      type="submit"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                     >
-                      Imagen
-                    </label>
-
-                    <input
-                      type="text"
-                      placeholder="Image"
-                      value={image}
-                      onChange={(e) => setImage(e.target.value)}
-                    ></input>
-
-                    <input
-                      label="Choose file"
-                      type="file"
-                      onChange={uploadFileHandler}
-                    ></input>
-                  </form>
+                      Guardar
+                    </button>
+                  </div>
                 </div>
-                <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                  <button
-                    type="submit"
-                    className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                  >
-                    Guardar
-                  </button>
-                </div>
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
-    )}
-  </>
-);
+      )}
+    </>
+  );
+}
