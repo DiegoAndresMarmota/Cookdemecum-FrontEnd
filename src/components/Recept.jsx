e74d759176c5dd333dd565aae9b6a0e38ff3d3c4
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-12-01 20:27:06 -0300

    Corrección de API externa

--- src/components/Recept.jsx
+++ src/components/Recept.jsx
@@ -1,26 +1,34 @@
 import React, { useEffect, useState } from "react";
 import Receipts from "./food/Receipts";
 
-const APIKEY = "27173db733ae473fbe67ecfd466f2d62";
+// const APIKEY = "27173db733ae473fbe67ecfd466f2d62";
+// const url = `https://api.spoonacular.com/recipes/complexSearch?query=pasta&maxFat=25&number=2&apiKey=${APIKEY}`;
+const URL = "https://api-foodrecipes.azurewebsites.net/recipes";
+
+const requestOptions = {
+  // mode: "no-cors",
+  method: "GET",
+  // redirect: "follow",
+  // headers: {
+  //   "Access-Control-Allow-Origin": "*",
+  // },
+};
 
 export default function Recept() {
   const [menu, setMenu] = useState([]);
 
   useEffect(() => {
-    fetch(
-      `https://api.spoonacular.com/recipes/complexSearch?query=pasta&maxFat=25&number=2&apiKey=${APIKEY}`
-    )
+    fetch(URL, requestOptions)
       .then((response) => response.json())
-      .then((response) => {
-        console.log(response.results);
-        setMenu(response.results);
+      .then((data) => {
+        setMenu(data);
       })
       .catch((err) => console.error(err));
   }, []);
 
   return (
-    <div className="container">
-      <div className="row">
+    <div className="flex">
+      <div className="flex gap-8">
         {menu.map((menuItem) => (
           <Receipts {...menuItem} />
         ))}
