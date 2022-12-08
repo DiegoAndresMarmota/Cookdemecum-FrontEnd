432d0762b5e6c713fcedc4bf9e0020b1c171b50c
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-12-08 19:41:10 -0300

    Corrección de AddBlog

--- src/components/AddBlog.jsx
+++ src/components/AddBlog.jsx
@@ -6,76 +6,91 @@ import Messages from "./Messages";
 import Loader from "./Loader";
 
 export default function AddBlog() {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const path = "/";
-    const blogCreate = useSelector((state) => state.blogCreate);
-    const { loading, error } = blogCreate;
-    const [body, setBody] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const path = "/";
+  const blogCreate = useSelector((state) => state.blogCreate);
+  const { loading, error } = blogCreate;
+  const [body, setBody] = useState("");
+  const [title, setTitle] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        dispatch(createBlogAction(body));
-        dispatch(listBlogs());
-        navigate(path);
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(createBlogAction(title, body));
+    navigate(path);
+  };
 
-    return (
-        <>
-            {loading ? (
-                <Loader />
-            ) : error ? (
-                <Messages>{error}</Messages>
-            ) : (
-                <div>
-                    <div className="md:grid md:grid-cols-4 md:gap-6">
-                        <div className="md:col-span-1"></div>
-                        <div className="mt-5 md:col-span-2 md:mt-0">
-                            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-                                Crear un post
-                            </h2>
-                            <form onSubmit={handleSubmit}>
-                                <div className="shadow sm:overflow-hidden sm:rounded-md">
-                                    <div className="space-y-6 bg-white px-4 py-5 sm:p-6">
-                                        <div className="grid grid-cols-3 gap-6">
-                                            <div className="col-span-3 sm:col-span-2"></div>
-                                        </div>
-
-                                        <div>
-                                            <label
-                                                htmlFor="about"
-                                                className="block text-sm font-medium text-gray-700"
-                                            >
-                                                Acerca
-                                            </label>
-                                            <div className="mt-1">
-                                                <textarea
-                                                    value={body}
-                                                    onChange={(e) => setBody(e.target.value)}
-                                                    type="text"
-                                                    id="body"
-                                                    rows={3}
-                                                    className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm"
-                                                    placeholder="Escribe aqui!"
-                                                />
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                                        <button
-                                            type="submit"
-                                            className="inline-flex justify-center rounded-md border border-transparent bg-red-700 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-red-800 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
-                                        >
-                                            Publicar
-                                        </button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
+  return (
+    <>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Messages>{error}</Messages>
+      ) : (
+        <div>
+          <div className="md:grid md:grid-cols-4 md:gap-6">
+            <div className="md:col-span-1"></div>
+            <div className="mt-5 md:col-span-2 md:mt-0">
+              <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+                Crear un post
+              </h2>
+              <form onSubmit={handleSubmit}>
+                <div className="shadow sm:overflow-hidden sm:rounded-md">
+                  <div className="space-y-6 bg-white px-4 py-5 sm:p-6">
+                    <div className="grid grid-cols-3 gap-6">
+                      <div className="col-span-3 sm:col-span-2"></div>
                     </div>
+
+                    <div>
+                      <label
+                        htmlFor="about"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Título
+                      </label>
+                      <input
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        id="title"
+                        className="bg-white w-full border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+                        placeholder="¿Cuál es el título?"
+                        type="text"
+                        name="title"
+                      />
+                      <label
+                        htmlFor="about"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Publicación
+                      </label>
+                      <div className="mt-1">
+                        <textarea
+                          value={body}
+                          onChange={(e) => setBody(e.target.value)}
+                          type="text"
+                          id="comentary"
+                          rows={3}
+                          className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm"
+                          placeholder="Escribe aqui!"
+                        />
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                    <button
+                      type="submit"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-red-700 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-red-800 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
+                    >
+                      Publicar
+                    </button>
+                  </div>
                 </div>
-            )}
-        </>
-    );
-};
+              </form>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
