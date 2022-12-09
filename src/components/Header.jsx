0b6252d70a016e063be2dffb968bb0de68adefd7
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-12-09 01:30:02 -0300

    Corrección del estilo de userInfo.data.username

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -53,7 +53,9 @@ export default function Header() {
                   <AiFillPlusSquare size={30} />
                 </a>
 
-                <h3>{userInfo.data.username}</h3>
+                <h3 className="bg-red-600 py-1 px-5 text-sm font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 rounded mt-1">
+                  {userInfo.data.username}
+                </h3>
 
                 <Menu as="div" className="relative ml-3">
                   <div>
