eac8edd6382f0580232a7a3960ded02f02918b3c
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-12-09 01:56:40 -0300

    Edición AddBlog

--- src/components/AddBlog.jsx
+++ src/components/AddBlog.jsx
@@ -28,11 +28,11 @@ export default function AddBlog() {
         <Messages>{error}</Messages>
       ) : (
         <div>
-          <div className="md:grid md:grid-cols-4 md:gap-6">
+          <div className="md:grid md:grid-cols-4 md:gap-6 bg-red-100 pb-40">
             <div className="md:col-span-1"></div>
             <div className="mt-5 md:col-span-2 md:mt-0">
-              <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-                Crear un post
+              <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900 pb-5">
+                Mi receta
               </h2>
               <form onSubmit={handleSubmit}>
                 <div className="shadow sm:overflow-hidden sm:rounded-md">
@@ -41,27 +41,27 @@ export default function AddBlog() {
                       <div className="col-span-3 sm:col-span-2"></div>
                     </div>
 
-                    <div>
+                    <div className="rounded">
                       <label
                         htmlFor="about"
-                        className="block text-sm font-medium text-gray-700"
+                        className="block text-sm font-medium text-gray-700 my-3"
                       >
-                        Título
+                        Mi receta se llama:
                       </label>
                       <input
                         value={title}
                         onChange={(e) => setTitle(e.target.value)}
                         id="title"
-                        className="bg-white w-full border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-                        placeholder="¿Cuál es el título?"
+                        className="bg-white w-full border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm my-1"
+                        placeholder="¿Cuál es el nombre de la receta?"
                         type="text"
                         name="title"
                       />
                       <label
                         htmlFor="about"
-                        className="block text-sm font-medium text-gray-700"
+                        className="block text-sm font-medium text-gray-700 mt-4 mb-4"
                       >
-                        Publicación
+                        Preparación de la receta:
                       </label>
                       <div className="mt-1">
                         <textarea
@@ -71,7 +71,7 @@ export default function AddBlog() {
                           id="comentary"
                           rows={3}
                           className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm"
-                          placeholder="Escribe aqui!"
+                          placeholder="¿Cuáles son tus ingredientes? ¿Cómo lo preparas?"
                         />
                       </div>
                     </div>
