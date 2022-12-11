233f7477b7802e54938131095b06eadba4077332
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-12-11 18:49:58 -0300

    Actualización de iconos de Navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -47,7 +47,7 @@ export default function Header() {
               <Popover.Group as="nav" className="hidden space-x-10 md:flex">
                 <a
                   href="/"
-                  className="text-base font-medium text-gray-500 hover:text-gray-900"
+                  className="text-base font-medium text-gray-500 hover:text-gray-900 pt-1"
                 >
                   <AiFillHome size={30} />
                 </a>
