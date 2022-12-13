935274ce541bc63a01a6a7aa74d62f89cfffbeec
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-12-12 21:23:19 -0300

    Alineación final del Navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -53,7 +53,7 @@ export default function Header() {
                 </a>
                 <a
                   href="/addBlog"
-                  className="text-base font-medium text-gray-500 hover:text-gray-900"
+                  className="text-base font-medium text-gray-500 hover:text-gray-900 pt-1.5"
                 >
                   <AiFillPlusSquare size={30} />
                 </a>
