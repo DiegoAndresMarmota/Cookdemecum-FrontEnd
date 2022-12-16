9bffed1446e8204775e78f26b38a2fc41f88450d
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-12-16 20:07:07 -0300

    Actualización

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -33,7 +33,9 @@ export default function Header() {
                 alt="icono-cook-demecum"
               />
             </a>
-            <span className=" flex items-center font-bold pl-2 text-2xl">COOK-DEMECUM</span>
+            <span className=" flex items-center font-bold pl-2 text-2xl">
+              COOK-DEMECUM
+            </span>
           </div>
           <div className="-my-2 -mr-2 md:hidden">
             <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-red-500">
